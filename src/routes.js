250b9dfbedc8a796375d466a4675b4f6e23f8571
willianproni <pronifwillian@gmail.com>
2025-07-27 15:07:57 -0300

    Feat Test - Middlewares

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,15 @@ const ContactController = require('./app/controllers/ContactController');
 
 const router = Router();
 
-router.get('/contacts', ContactController.index);
+router.get(
+  '/contacts',
+  (req, res, next) => {
+    req.appId = 'middlewares';
+
+    next();
+  },
+  ContactController.index
+);
 router.get('/contacts/:id', ContactController.show);
 router.delete('/contacts/:id', ContactController.delete);
 
