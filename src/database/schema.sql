072d3d10576d2c2aeec78a272e94a74844eea72a
willianproni <pronifwillian@gmail.com>
2025-07-28 08:22:13 -0300

    Feat - Created schema SQL (mycontacts)

---
+++ src/database/schema.sql
@@ -0,0 +1,19 @@
+-- SQL universal linguage database relation
+
+CREATE DATABASE mycontacts;
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS categories (
+  id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
+  name VARCHAR NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS CONTACTS (
+  id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
+  name VARCHAR NOT NULL,
+  email VARCHAR UNIQUE,
+  phone VARCHAR,
+  category_id UUID,
+  FOREIGN KEY(category_id) REFERENCES categories(id)
+);
